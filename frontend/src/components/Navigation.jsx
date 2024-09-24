83533177cc3092349423e425ceb329c368707b1e
allysanfoot <allysafoot@gmail.com>
2024-09-23 20:05:53 -0700

    feat: getting starting pages set up

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/navigation.css'
+
+import Login from './Login'
+
+function Navigation() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+                <Login />
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+function Home() {
+    return (
+        <div>
+            <Navigation />
+            Home
+        </div>
+    )
+}
+
+export default Home
